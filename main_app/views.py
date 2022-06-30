9d9469a47d5dbcf171b9ae17f3aa85c6f32e2869
Romeo Encinares <romeoncrs@gmail.com>
2022-06-30 18:29:26 +0800

    Create register user function with usercreationform()

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('index')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'register.html', context)
