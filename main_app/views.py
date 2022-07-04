95d6a955ca873312a1480da4c74749e72c987c34
Romeo Encinares <romeoncrs@gmail.com>
2022-07-04 20:47:03 +0800

    Create loginPage function

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
 def index(request):
+    current_user = request.user
+    print(current_user)
     return render(request, 'index.html')
 
 def register(request):
@@ -19,3 +21,17 @@ def register(request):
     }
 
     return render(request, 'register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            print('Success')
+            redirect('index')
+            
+    return render(request, 'login.html')
