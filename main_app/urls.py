22121232fbe29c792f4ca87ba8a6132ffa32c06b
Romeo Encinares <romeoncrs@gmail.com>
2022-07-04 20:47:30 +0800

    Add loginPage url route

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, register
+from .views import index, register, loginPage
 
 urlpatterns = [
     path('', index, name='index'),
     path('register', register, name='register'),
+    path('login', loginPage, name='login'),
 ]
