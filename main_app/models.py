49c7fd36f3e32b6a7912d0110a686e5879c9b37c
Romeo Encinares <romeoncrs@gmail.com>
2022-07-11 18:59:00 +0800

    Add balance field and addTransaction method to user profile

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 
 class userProfile(models.Model):
     username = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.FloatField(default=0)
+
+    def addTransaction(self, type, cost):
+        if type == "savings":
+            self.balance += cost
+        else:
+            self.balance -= cost
 
     def __str__(self):
         return str(self.username)
