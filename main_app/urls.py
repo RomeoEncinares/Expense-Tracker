98f4601edbed46c57affc580ccdfeb0392fcfe36
Romeo Encinares <romeoncrs@gmail.com>
2022-07-22 19:25:22 +0800

    Add homePage in urlpatterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, register, loginPage
+from .views import index, register, loginPage, homePage
 
 urlpatterns = [
     path('', index, name='index'),
     path('register', register, name='register'),
     path('login', loginPage, name='login'),
+    path('home', homePage, name='home'),
 ]
