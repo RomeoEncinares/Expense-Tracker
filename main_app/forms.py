fcc28f0a5eff7545622ea4e21d379e6368e63932
Romeo Encinares <romeoncrs@gmail.com>
2022-08-15 21:30:49 +0800

    Change transaction from forms.Forms to forms.ModelForm

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import transaction
 
-class transactionForm(forms.Form):
+class transactionForm(forms.ModelForm):
     TRANSACTION_CHOICES = (('S', 'Savings'), ('E', 'Expenses'))
     transactionType = forms.ChoiceField(widget=forms.RadioSelect, choices=TRANSACTION_CHOICES)
     amount = forms.FloatField()
