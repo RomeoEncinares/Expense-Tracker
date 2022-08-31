e5a337b3f5c563766d3ff63787001e6a7c88adaa
Romeo Encinares <romeoncrs@gmail.com>
2022-08-31 18:44:11 +0800

    Add attributes to transactionType and amount

--- main_app/forms.py
+++ main_app/forms.py
@@ -3,8 +3,8 @@ from .models import transaction
 
 class transactionForm(forms.ModelForm):
     TRANSACTION_CHOICES = (('S', 'Savings'), ('E', 'Expenses'))
-    transactionType = forms.ChoiceField(widget=forms.RadioSelect, choices=TRANSACTION_CHOICES)
-    amount = forms.FloatField()
+    transactionType = forms.ChoiceField(widget=forms.RadioSelect(attrs={'id':'transactionType', 'style':'list-style:none'}), choices=TRANSACTION_CHOICES)
+    amount = forms.FloatField(widget=forms.NumberInput(attrs={'class':'form-control', 'id':'amount'}))
     
     class Meta:
         model = transaction
