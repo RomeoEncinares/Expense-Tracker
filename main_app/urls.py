29bbb8723b073f64febfb0d08a1cf368bd49bc99
Romeo Encinares <romeoncrs@gmail.com>
2022-09-05 19:39:51 +0800

    Add transactionView in urlpatterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, register, loginView, homeView
+from .views import index, register, loginView, homeView, transactionsView
 
 urlpatterns = [
     path('', index, name='index'),
     path('register', register, name='register'),
     path('login', loginView, name='login'),
     path('home', homeView, name='home'),
+    path('transactions', transactionsView, name='transactions')
 ]
