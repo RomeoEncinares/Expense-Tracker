b23ab913607701fec7503a0b8bd0d6bb4287af1a
Romeo Encinares <romeoncrs@gmail.com>
2022-09-07 17:47:29 +0800

    Add transactionCategory and CATEGORY_CHOICES

--- main_app/models.py
+++ main_app/models.py
@@ -18,7 +18,21 @@ class transaction(models.Model):
     username = models.ForeignKey(userProfile, on_delete=models.CASCADE)
     TRANSACTION_CHOICES = (('S', 'Savings'), ('E', 'Expenses'))
     transactionType = models.CharField(max_length=1, choices=TRANSACTION_CHOICES)
+    CATEGORY_CHOICES = (
+        ('FD', 'FOOD & DRINKS'),
+        ('SH', 'SHOPPING'),
+        ('HO', 'HOUSING'),
+        ('TR', 'TRANSPORTATION'),
+        ('VE', 'VEHICLE'),
+        ('LE', 'LIFE & ENTERTAINMENT'),
+        ('PC', 'PC, COMMUNICATION'),
+        ('FE', 'FINANCIAL EXPENSES'),
+        ('IV', 'INVESTMENT'),
+        ('IC', 'INCOME'),
+        ('OT', 'OTHERS'),
+    )
+    transactionCategory = models.CharField(max_length=2, choices=CATEGORY_CHOICES)
     amount = models.FloatField()
 
     def __str__(self):
-        return "{} {} {:.2f}".format(self.username, self.transactionType, self.amount)
+        return "{} {} {} {:.2f}".format(self.username, self.transactionType, self.transactionCategory, self.amount)
