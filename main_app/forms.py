e59b416f962603f11f9298c3baea3d50aa6ce360
Romeo Encinares <romeoncrs@gmail.com>
2022-09-07 18:32:02 +0800

    Add transactionCategory in transactionForm

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,8 +4,23 @@ from .models import transaction
 class transactionForm(forms.ModelForm):
     TRANSACTION_CHOICES = (('Income', 'Income'), ('Expense', 'Expense'))
     transactionType = forms.ChoiceField(widget=forms.RadioSelect(attrs={'id':'transactionType', 'style':'list-style:none'}), choices=TRANSACTION_CHOICES)
+    CATEGORY_CHOICES = (
+        ('Food & Drinks', 'Food & Drinks'),
+        ('Shoppin', 'Shopping'),
+        ('Housing', 'Housing'),
+        ('Transportation', 'Transportation'),
+        ('Vehicle', 'Vehicle'),
+        ('Life & Entertainment', 'Life & Entertainment'),
+        ('PC, Communication', 'PC, Communication'),
+        ('Financial Investment', 'Financial Investment'),
+        ('Investment', 'Investment'),
+        ('Income', 'Income'),
+        ('Others', 'Others'),
+    )
+    transactionCategory = forms.ChoiceField(widget=forms.RadioSelect(attrs={'id':'transactionType', 'style':'list-style:none'}), choices=CATEGORY_CHOICES)
+
     amount = forms.FloatField(widget=forms.NumberInput(attrs={'class':'form-control', 'id':'amount'}))
     
     class Meta:
         model = transaction
-        fields = ['transactionType', 'amount']
+        fields = ['transactionType', 'transactionCategory', 'amount']
