996f4636dbc772eb6538dde3a93998891a933590
Romeo Encinares <romeoncrs@gmail.com>
2022-09-07 18:22:59 +0800

    Change transactionChoices to Income and Expense

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import transaction
 
 class transactionForm(forms.ModelForm):
-    TRANSACTION_CHOICES = (('S', 'Savings'), ('E', 'Expenses'))
+    TRANSACTION_CHOICES = (('Income', 'Income'), ('Expense', 'Expense'))
     transactionType = forms.ChoiceField(widget=forms.RadioSelect(attrs={'id':'transactionType', 'style':'list-style:none'}), choices=TRANSACTION_CHOICES)
     amount = forms.FloatField(widget=forms.NumberInput(attrs={'class':'form-control', 'id':'amount'}))
     
