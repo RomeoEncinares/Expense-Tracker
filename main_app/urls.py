acf384dabbafa9503d51ba856e8b85742da73635
Romeo Encinares <romeoncrs@gmail.com>
2022-09-21 19:26:23 +0800

    Add statisticsView in urlpatterns

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index, register, loginView, homeView, transactionsView
+from .views import index, register, loginView, homeView, transactionsView, statisticsView
 
 urlpatterns = [
     path('', index, name='index'),
     path('register', register, name='register'),
     path('login', loginView, name='login'),
     path('home', homeView, name='home'),
-    path('transactions', transactionsView, name='transactions')
+    path('transactions', transactionsView, name='transactions'),
+    path('statistics', statisticsView, name='statistics'),
 ]
